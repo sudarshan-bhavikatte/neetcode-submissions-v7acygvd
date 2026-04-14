bc344b231b26effa83571c21bae72b609afb6942
sudarshan-bhavikatte <149920240+sudarshan-bhavikatte@users.noreply.github.com>
2026-04-14 12:00:37 +0530

    Add: longest-turbulent-subarray - submission-2

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-2.cpp
@@ -0,0 +1,37 @@
+/*
+    1. return max(len of subarray) where :
+        - index k >= i and k < j
+        - if arrp[k] > arr[k+1], k must be odd
+        - if arrp[k] < arr[k+1], k must be even
+        - or 
+        - vice versa
+*/
+
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& arr) {
+        int l = 0, r = 1, res = 1;
+
+        string prev = " ";
+
+        while(r < arr.size()){
+            if(arr[r-1] > arr[r] && prev != ">"){
+                res = max(res, r - l + 1);
+                r++;
+                prev = ">";
+            }
+            else if (arr[r-1] < arr[r] && prev != "<"){
+                res = max(res, r- l + 1);
+                r++;
+                prev = "<";
+            }
+            else {
+                r = (arr[r] == arr[ r - 1]) ? r + 1 : r;
+                l = r - 1;
+                prev = " ";
+            }
+        }
+
+        return res;
+    }
+};
