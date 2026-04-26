f8f0d19ffdae94bffb7c3c7479713daccd97e60b
sudarshan-bhavikatte <149920240+sudarshan-bhavikatte@users.noreply.github.com>
2026-04-26 07:44:36 +0530

    Add: dijkstra - submission-3

---
+++ Data Structures & Algorithms/dijkstra/submission-3.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    unordered_map<int, int> shortestPath(int n, vector<vector<int>>& edges, int src) {
+        unordered_map<int, int> res;
+
+        unordered_map<int, vector<pair<int, int>>> adj;
+
+        for(vector<int> e : edges){
+            adj[e[0]].push_back({e[1], e[2]});
+        }
+
+        priority_queue<
+            pair<int,int>, 
+            vector<pair<int,int>>, 
+            greater<pair<int, int>>
+            > pq;
+
+        pq.push({0,src});
+        
+
+        while(!pq.empty()){
+            pair<int, int> top = pq.top();
+            pq.pop();
+
+            int w = top.first;
+            int node = top.second;
+
+            if(res.find(node) != res.end()) continue;
+            res[node] = w;
+
+            for(auto a : adj[node]){
+                if(res.find(a.first) == res.end()) pq.push({a.second + w, a.first});
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (res.find(i) == res.end()) {
+                res[i] = -1;
+            }
+        }
+
+
+        return res;
+    }
+};
