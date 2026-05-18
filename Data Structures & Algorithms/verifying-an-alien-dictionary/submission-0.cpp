61caa05a5acf200f998a32d2c0ba91b87222e854
Sudharshan <149920240+sudarshan-bhavikatte@users.noreply.github.com>
2026-05-18 21:57:32 +0530

    Add: verifying-an-alien-dictionary - submission-0

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    unordered_map<char, int> hash;
+    bool helper(string word1, string word2){
+        int i = 0;
+        int j = 0;
+
+        while(i < word1.size() && j < word2.size() && hash[word1[i]] == hash[word2[j]]){
+            cout << word1[i] <<  " = " << word2[j] << endl;
+            i++; j++;
+        }
+
+        if(i < word1.size() && j < word2.size()){
+            if(hash[word1[i]] < hash[word2[j]]){
+
+                cout << word1[i] <<  " < " << word2[j] << endl << endl;
+                return true;
+            }
+            else {
+                cout << word1[i] <<  " > " << word2[j] << endl << endl;
+                return false;
+            }
+        } 
+        cout << "true" << endl;
+        return word1.size() < word2.size();
+    }
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        
+        for(int i = 0; i < order.size(); i++) hash[order[i]] = i;
+
+        for(int i = 1; i < words.size(); i++){
+            if(!helper(words[i - 1], words[i])) return false;
+        }
+
+        return true;
+    }
+};
