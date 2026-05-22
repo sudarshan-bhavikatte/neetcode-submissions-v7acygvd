ee1b4d8b0ac21514d641502bbabe7cf307d984ff
Sudharshan <149920240+sudarshan-bhavikatte@users.noreply.github.com>
2026-05-22 18:18:30 +0530

    Add: prototype - submission-0

---
+++ Design Patterns/prototype/submission-0.cpp
@@ -0,0 +1,54 @@
+class Shape {
+public:
+    virtual ~Shape() {}
+    virtual Shape* clone() const = 0;
+};
+
+class Rectangle : public Shape {
+private:
+    int width;
+    int height;
+
+public:
+    Rectangle(int w, int h) : width(w), height(h) {}
+
+    int getWidth() const {
+        return width;
+    }
+
+    int getHeight() const {
+        return height;
+    }
+
+    Shape* clone() const override {
+        
+        return new Rectangle(this->width, this->height);
+    }
+};
+
+class Square : public Shape {
+private:
+    int length;
+
+public:
+    Square(int l) : length(l) {}
+
+    int getLength() const {
+        return length;
+    }
+
+    Shape* clone() const override {
+        return new Square(this->length);
+    }
+};
+
+class Test {
+public:
+    vector<Shape*> cloneShapes(const vector<Shape*>& shapes) {
+        vector<Shape*> clones;
+        for(Shape* s : shapes){
+            clones.push_back(s->clone());
+        }
+        return clones;
+    }
+};
